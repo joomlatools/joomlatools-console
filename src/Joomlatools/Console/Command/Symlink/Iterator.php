bef01f859c28f3fa0afaec660a63486c5d0b964c
Ercan Ozkaya <ercan@timble.net>
2013-08-09 16:55:44 +0300

    Add symlinker

---
+++ src/Joomlatools/Console/Command/Symlink/Iterator.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Joomlatools\Console\Command\Symlink;
+
+class Iterator extends \RecursiveIteratorIterator
+{
+    protected $source;
+    protected $target;
+
+    /**
+     * @param string $source Source dir (usually from an IDE workspace)
+     * @param string $target Target dir (usually where a joomla installation resides)
+     */
+    public function __construct($source, $target)
+    {
+        $this->source = $source;
+        $this->target = $target;
+
+        parent::__construct(new \RecursiveDirectoryIterator($source));
+    }
+
+    public function callHasChildren()
+    {
+        $filename = $this->getFilename();
+        if ($filename[0] == '.') {
+            return false;
+        }
+
+        $source = $this->key();
+
+        $target = str_replace($this->source, '', $source);
+        $target = str_replace('/site', '', $target);
+        $target = $this->target.$target;
+
+        if (is_link($target)) {
+            unlink($target);
+        }
+
+        if (!is_dir($target))
+        {
+            $this->createLink($source, $target);
+            return false;
+        }
+
+        return parent::callHasChildren();
+    }
+
+    public function createLink($source, $target)
+    {
+        if (!file_exists($target)) {
+            `ln -sf $source $target`;
+        }
+    }
+}
