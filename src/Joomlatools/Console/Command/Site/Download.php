cba769e9b491b8a22444c09e6735a48ccf2dfc56
Steven Rombauts <steven@timble.net>
2015-10-27 11:16:36 +0000

    Remove forward slashes from tarball paths

--- src/Joomlatools/Console/Command/Site/Download.php
+++ src/Joomlatools/Console/Command/Site/Download.php
@@ -152,9 +152,12 @@ class Download extends AbstractSite
     protected function _getTarball(OutputInterface $output)
     {
         $tar   = $this->version.'.tar.gz';
+        // Replace forward slashes with a dash, otherwise the path looks like it contains more subdirectories
+        $tar = str_replace('/', '-', $tar);
+
         $cache = $this->versions->getCacheDirectory().'/'.$tar;
 
-        if(file_exists($cache) && !$this->versions->isBranch($this->version)) {
+        if (file_exists($cache) && !$this->versions->isBranch($this->version)) {
             return $cache;
         }
 
