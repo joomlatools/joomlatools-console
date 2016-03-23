71abc1d83f06d9435599489eaba10da2d6b86d8e
Ercan Ozkaya <ercan@timble.net>
2016-03-23 17:17:52 +0200

    Bump version number to 1.4.5

--- src/Joomlatools/Console/Application.php
+++ src/Joomlatools/Console/Application.php
@@ -11,7 +11,7 @@ class Application extends \Symfony\Component\Console\Application
      *
      * @var string
      */
-    const VERSION = '1.4.4';
+    const VERSION = '1.4.5';
 
     /**
      * Application name
