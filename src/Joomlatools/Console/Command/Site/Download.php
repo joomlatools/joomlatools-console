978451145d6c3450c317f0fcc7517da88df7de09
Steven Rombauts <steven@timble.net>
2017-11-20 12:55:29 +0000

    #75: No longer use bare clones in order to deal with Git submodules

--- src/Joomlatools/Console/Command/Site/Download.php
+++ src/Joomlatools/Console/Command/Site/Download.php
@@ -281,6 +281,10 @@ class Download extends AbstractSite
      */
     protected function _clone($target)
     {
+        if (substr($target, -3) == '.gz') {
+            $target = substr($target, 0, -3);
+        }
+
         $clone      = $this->versions->getCacheDirectory() . '/source';
         $repository = $this->versions->getRepository();
 
@@ -288,21 +292,43 @@ class Download extends AbstractSite
         {
             $this->output->writeln("<info>Cloning $repository - this could take a few minutes ..</info>");
 
-            `git clone --recursive --bare --mirror "$repository" "$clone"`;
+            `git clone --recursive "$repository" "$clone"`;
         }
 
         if ($this->versions->isBranch($this->version))
         {
             $this->output->writeln("<info>Fetching latest changes from $repository - this could take a few minutes ..</info>");
 
-            `git --git-dir "$clone" --bare fetch`;
+            `git --git-dir "$clone/.git" fetch`;
         }
 
         $this->output->writeln("<info>Creating $this->version archive for $repository ..</info>");
 
-        `git --git-dir "$clone" archive --prefix=base/ $this->version | gzip >"$target"`;
+        `git --git-dir "$clone/.git" archive --prefix=base/ $this->version >"$target"`;
 
-        return (bool) @filesize($target);
+        // Make sure to include submodules
+        if (file_exists("$clone/.gitmodules"))
+        {
+            exec("cd $clone && (git submodule foreach) | while read entering path; do echo \$path; done", $output, $return_var);
+
+            if (is_array($output))
+            {
+                foreach ($output as $module)
+                {
+                    $module = trim($module, "'");
+                    $path   = "$clone/$module";
+
+                    $cmd = "cd $path && git archive --prefix=base/$module/ HEAD > /tmp/$module.tar && tar --concatenate --file=\"$target\" /tmp/$module.tar";
+                    exec($cmd);
+
+                    @unlink("/tmp/$module.tar");
+                }
+            }
+        }
+
+        `gzip $target`;
+
+        return (bool) @filesize("$target.gz");
     }
 
     /**
