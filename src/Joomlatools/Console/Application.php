165354fdf7ff6054db4dc2fa0b63dd60b092a880
Steven Rombauts <steven@kotuha.be>
2020-05-29 10:24:27 +0200

    Update Application version to v1.5.7

--- src/Joomlatools/Console/Application.php
+++ src/Joomlatools/Console/Application.php
@@ -11,7 +11,7 @@ class Application extends \Symfony\Component\Console\Application
      *
      * @var string
      */
-    const VERSION = '1.5.5';
+    const VERSION = '1.5.7';
 
     /**
      * Application name
